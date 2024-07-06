ca08451e905e6948bfc51542042432f8a5125bc0
David-Buchukuri <datobuchukuri20@gmail.com>
2024-07-06 20:35:16 +0400

    program to find max value in set of numbers

---
+++ chapter-3/max.asm
@@ -0,0 +1,52 @@
+#PURPOSE: This program finds the maximum number of a set of data items.
+
+# VARIABLES: The registers have the following uses:
+# %edi - Holds the index of the data item being examined
+# %ebx - Largest data item found
+# %eax - Current data item
+#
+# The following memory locations are used:
+# data_items - contains the item data. A 0 is used to terminate the data
+
+.section .data
+
+data_array: 
+.long 1, 2, 9, 5, 2, 9, 25, 0
+
+.section .text
+.global _start
+
+
+# %edi - Holds the index of the data item being examined
+# %ebx - Largest data item found
+# %eax - Current data item
+
+_start:
+
+movl $0, %edi
+movl data_array(, %edi, 4), %ebx
+movl %ebx, %eax
+
+jmp loop
+
+update_max_element:
+    movl %eax, %ebx
+    jmp iterate
+
+
+loop:
+    cmpl $0, %eax
+    je exit
+
+    cmpl %ebx, %eax
+    jg update_max_element
+    
+    iterate:
+    incl %edi
+    movl data_array(, %edi, 4), %eax
+    jmp loop
+
+
+exit:
+    movl $1, %eax
+    int $0x80
