f3c3a7c989ced70a375a787881f638b2cf5926ac
David-Buchukuri <datobuchukuri20@gmail.com>
2024-07-12 02:49:45 +0400

    script for assembling, linking and running assembly files

---
+++ clr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <filename.asm>"
+  exit 1
+fi
+
+filename="${1%.asm}"
+
+as --32 "$filename.asm" -o "$filename.o"
+ld -m elf_i386 "$filename.o" -o "$filename"
+./"$filename"
